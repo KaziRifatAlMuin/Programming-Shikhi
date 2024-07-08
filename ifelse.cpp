83854f8442901018c331831e43f57ca5efbec24e
Kazi Rifat Al Muin <kazirifatalmuin246@gmail.com>
2024-07-09 04:25:14 +0600

    Programming Shikhi

---
+++ ifelse.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    if(x > 10) cout << "YES\n";
+    else  cout << "NO\n";
+    cout << "END\n";
+    return 0;
+}
---
+++ ifelseladder.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    scanf("%d", &x);
+    if(x == 1) printf("one\n");
+    else if(x == 2) printf("two\n");
+    else if(x == 3) printf("three\n");
+    else if(x == 4) printf("four\n");
+    else if(x == 5) printf("five\n");
+    else if(x == 6) printf("six\n");
+    else if(x == 7) printf("seven\n");
+    else if(x == 8) printf("eight\n");
+    else if(x == 9) printf("nine\n");
+    else printf("Greater than 9\n");
+
+    return 0;
+}
